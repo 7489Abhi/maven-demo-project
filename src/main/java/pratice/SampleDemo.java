a11c42e291ffc4022c9163bd7826555faefac3ca
user <user@DESKTOP-14R4M77>
2022-09-16 18:28:34 +0530

    commit and push by user

---
+++ src/main/java/pratice/SampleDemo.java
@@ -0,0 +1,14 @@
+package pratice;
+
+import org.testng.annotations.Test;
+
+
+public class SampleDemo {
+	@Test
+	public void Demo()
+	{
+	System.out.println("this is a demo");
+
+	}
+}
+
---
+++ src/test/java/sample_tests/Sampletest.java
@@ -0,0 +1,18 @@
+package sample_tests;
+
+import org.testng.annotations.Test;
+
+public class Sampletest {
+
+	@Test
+	public void sampletest()
+	{
+		System.out.println("This is sample");
+	}
+	@Test
+	public void execution()
+	{
+		System.out.println("excution successfuly");
+
+	}
+}
